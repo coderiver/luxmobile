1891860a7201bf615dbe80b0049273fe2ec3798e
Valera19900 <kaktuz34@gmail.com>
2014-06-26 12:37:39 +0300

    upd

--- js/init.js
+++ js/init.js
@@ -1,1 +1,1 @@
-head.load("js/lib/jquery.js", "js/lib/jquery.cycle2.caption2.min.js", "js/lib/cycle.js", "js/common.js");
+head.load("js/lib/jquery.js", "js/lib/jquery.cycle2.swipe.js", "js/lib/cycle.js", "js/common.js");
