c6ddab5dfee8c3e21822dee8053ce16648c9c1df
akella <akella.a@gmail.com>
2014-06-26 23:07:02 +0300

    links from main page

--- js/common.js
+++ js/common.js
@@ -1,10 +1,21 @@
 head.ready(function() {
-
-
+	//$('.cycle-slideshow').cycle();
+	var window_hash = location.hash;
 
 	if (window_hash) {
 		//item.removeClass('is-active');
-		$(window_hash).addClass('is-active');
+		//alert(window_hash);
+		//$(window_hash).addClass('is-active');
+		
+		setTimeout(function(){
+			
+			el = $(window_hash);
+			aa = $('.cycle__item').index(el);
+			$('.cycle-slideshow').cycle('goto', aa-1);
+			//alert(aa+window_hash);
+			//console.log('aa');
+
+		}, 100);
 	};
 	var agent = navigator.userAgent,
 	event = (agent.match(/iPad/i)) ? "touchstart" : "click";
